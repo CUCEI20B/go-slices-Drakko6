5f718c54bdec1fbff5dbabbd9028e8c43d1d5926
César Tonatiuh Tamayo Ávila <cesar.tamayo@alumnos.udg.mx>
2020-10-08 16:58:02 -0500

    Ya captura enteros y realiza la suma

--- main.go
+++ main.go
@@ -3,5 +3,20 @@ package main
 import "fmt"
 
 func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+	var n uint64;
+
+	fmt.Scan(&n);
+
+	s:= make([]int64, 0, n);
+
+	var e int64;
+	var suma int64 = 0;
+	for i:=0; i<int(n); i++ {
+		
+		fmt.Scan(&e)
+		s = append(s, e)
+		suma += e;
+	}
+	fmt.Println(suma);
+
 }
